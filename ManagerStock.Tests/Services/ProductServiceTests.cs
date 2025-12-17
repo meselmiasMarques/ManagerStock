82a4a21d77c6221b15eeb3aace973b90155fa281
Meselmias Marques <meselmias.marques.lima@gmail.com>
2025-12-17 20:41:52 -0300

    ädicionado projeto test mstest

---
+++ ManagerStock.Tests/Services/ProductServiceTests.cs
@@ -0,0 +1,13 @@
+using ManagerStock.ViewModels.Product;
+
+namespace ManagerStock.Tests.Services;
+
+[TestClass]
+public sealed class ProductServiceTests
+{
+    [TestMethod]
+    public void CreateProductIsSucess()
+    {
+      
+    }
+}
---
+++ ManagerStock.Tests/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagerStock.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
